f3179ebc92a26e03503a676689289bd90a62c4ae
Debdeepd03 <debdeep22102003@gmail.com>
2024-09-11 09:50:37 +0530

    do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentService } from './student.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:[StudentService]
 })
 export class AppComponent {
   title = 'demo';
+  students:any[];
+  constructor(private s:StudentService){
+    this.students=this.s.getStudents();
+  }
 }
